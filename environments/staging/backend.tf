6f840770c064b59dbbc883c59e413968aacbacd1
Aaron King <aaronpking88@gmail.com>
2026-08-04 23:31:23 -0400

    Configured staging and prod backends

--- environments/staging/backend.tf
+++ environments/staging/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "group1-staging-terraform-state"
+    bucket = "group1-staging-terraform-state1112"
     key    = "staging/terraform.tfstate"
     region = "us-east-1"
   }
