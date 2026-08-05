a1fb25c6dc0c661c0db37b69318ca1eafa23f23a
Aaron King <aaronpking88@gmail.com>
2026-08-04 23:40:35 -0400

    Added autoscaling policies and CloudWatch alarms

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "group1-prod-terraform-state"
+    bucket = "group1-prod-terraform-state1112"
     key    = "prod/terraform.tfstate"
     region = "us-east-1"
   }
--- modules/asg/main.tf
+++ modules/asg/main.tf
@@ -12,9 +12,68 @@ resource "aws_autoscaling_group" "web" {
     var.target_group_arn
   ]
 
+
+
   launch_template {
     id = var.launch_template_id
     version = "$Latest"
   }
 
 }
+
+resource "aws_autoscaling_policy" "scale_out" {
+  name                   = "scale-out"
+  autoscaling_group_name = aws_autoscaling_group.web.name
+
+  adjustment_type   = "ChangeInCapacity"
+  scaling_adjustment = 1
+  cooldown          = 300
+}
+
+resource "aws_autoscaling_policy" "scale_in" {
+  name                   = "scale-in"
+  autoscaling_group_name = aws_autoscaling_group.web.name
+
+  adjustment_type   = "ChangeInCapacity"
+  scaling_adjustment = -1
+  cooldown          = 300
+}
+
+resource "aws_cloudwatch_metric_alarm" "cpu_high" {
+  alarm_name          = "cpu-high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 300
+  statistic           = "Average"
+  threshold           = 10
+
+  alarm_actions = [
+    aws_autoscaling_policy.scale_out.arn
+  ]
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.web.name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "cpu_low" {
+  alarm_name          = "cpu-low"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 300
+  statistic           = "Average"
+  threshold           = 5
+
+  alarm_actions = [
+    aws_autoscaling_policy.scale_in.arn
+  ]
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.web.name
+  }
+}
+
