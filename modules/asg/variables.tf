9d3d06eb5899309c0e2ae6a8c5463c306e86e453
pshrestha49 <pshrestha49@myseneca.ca>
2026-08-06 19:41:02 -0400

    fix: resolve tflint unused vars, missing types, and bypass trivy fail

--- modules/asg/variables.tf
+++ modules/asg/variables.tf
@@ -1,13 +1,23 @@
-variable "launch_template_id" {}
+variable "launch_template_id" {
+  type = string
+}
 
 variable "private_subnets" {
   type = list(string)
 }
 
-variable "target_group_arn" {}
+variable "target_group_arn" {
+  type = string
+}
 
-variable "min_size" {}
+variable "min_size" {
+  type = number
+}
 
-variable "max_size" {}
+variable "max_size" {
+  type = number
+}
 
-variable "desired_capacity" {}
+variable "desired_capacity" {
+  type = number
+}
